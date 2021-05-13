982db76142994bd4376b7970dad5b150b530810d
SourceRemedy <56258014+SourceRemedy@users.noreply.github.com>
2021-05-13 00:14:07 -0400

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-//empty
+document.getElementById("login")
+    .onsubmit = login
+
+function login(e) {
+    e.preventDefault()
+    const payload = {
+        body: JSON.stringify({
+          username: document.getElementById("username").value,
+          password: document.getElementById("password").value  
+        }),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch("/login", payload)
+        .then(res => res.json())
+        .then(res => console.log(res.body))
+        .catch(error => console.error(error))
+}
