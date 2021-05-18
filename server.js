e3840e17ac23ead9f93be2ebb49eb622c6678f8c
SourceRemedy <56258014+SourceRemedy@users.noreply.github.com>
2021-05-18 08:58:49 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,54 @@
 const express = require('express')
-const sqlite3 = require('sqlite3')
 const app = new express()
-const db = new sqlite3.Database(',/db/fullstack.db')
+const users = loadData().users
 
 app.use(express.static('public'))
 app.use(express.json())
 
 app.post("/login", (req, res) => {
     const user = req.body
-    const sql2 = "SELECT id, first_name, last_name FROM users WHERE username = ? AND password = ?"
-    db.all(sql2,[user.username, user.password],(err, rows) => {
-        if (rows && rows.length > 0) {
-            res.send({
-                message: "Successful login!",
-                user: rows[0]
-            })
-        }
-    
-
-
-    else {
-        if (user.username.length >= 4 && user.password.length >= 4) {
-            const sql = "INSERT INTO users (username, password, first_name, last_name) VALUES (?,?,?,?)"
-            db.run(sql,[user.username, user.password, user.firstName, user.lastName],(err) => {
-                if (err) console.error(err)
-                res.send({
-                    message: "Your account was successfully created.",
-                    userId: this.lastID
-                })
-            })
-        }
-        else {
-            res.status(401)
-            res.send({
-                message: "Username or password is invalid."
-            })
-        }
+    let userMatch = users.find( (u) => u.username == user.username && u.password == user.password)
+    if (userMatch) {
+        res.send({
+            message: "Successful login!",
+            userMatch
+        })
     }
+    else {
+    if (user.username.length >= 4 && user.password.length >= 4) {
+    const newUser = {
+        id: users.length+1,
+        username: user.username,
+        password: user.password,
+    }
+    users.push(newUser)
+    console.log(users)
+    res.send({
+        message: "Your account was successfully created.",
+        newUser
     })
+}
+else {
+    res.status(401)
+    res.send ({
+        message: "Username or password is invalid."
+    })
+}
+    }
 })
 
 
-
 app.listen(3000, () => console.log("Server started"))
+
+
+function loadUsers() {
+    return {
+        users:
+        [
+            {
+                username: "jword",
+                password: "hello2021"
+            }
+        ]
+    }
+}
