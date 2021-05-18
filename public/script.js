66e76d9fa6a6242adb9b13a121054506312bb4e9
SourceRemedy <56258014+SourceRemedy@users.noreply.github.com>
2021-05-18 12:27:56 -0400

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -8,7 +8,26 @@ document.getElementById("login")
     document.getElementById("createPost")
     .onsubmit = createPost
 
+    spawnPosts()
 
+
+    function createPost(e) {
+        e.preventDefault()
+        const payload = {
+            body: JSON.stringify({
+                text:document.getElementById("newPost").value
+            }),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        fetch("/posts", payload)
+        .then(res => res.json())
+        .then(res => console.log(res.body))
+        .catch(error => console.error(err))
+    }
+    
     function login(e) {
     e.preventDefault()
     const payload = {
@@ -30,6 +49,15 @@ document.getElementById("login")
 }
 
 function spawnPosts() {
-
+ fetch("/posts")
+ .then(res => res.json())
+ .then (posts => {
+     postsHTML = posts.map( post => `
+ <div class = "post">
+ <p>Catergory: ${posts.forum_id} <br/>
+ ${posts.content}</p>
+ </div>`).join(""),
+ $postsContainer.innerHTML = postsHTML})
+ .catch(err => console.error(err))
 }
 
